155f118dd8663890ea5c591243dcedc3929b590b
Ekaterina Epiphanova <epiph@yandex.ru>
2022-11-05 19:54:28 +0500

    feat: add protected rout

---
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+import { Navigate, useLocation } from 'react-router-dom';
+
+function ProtectedRoute({ component: Component, ...props }) {
+  const location = useLocation();
+  return (
+    props.loggedIn ? <Component {...props} /> : <Navigate to='/login' state={{from: location}} />  
+  );
+};
+
+export default ProtectedRoute;
