66fb5516cd6020339b5ae880abe587be8bf43975
Ekaterina Epiphanova <epiph@yandex.ru>
2022-11-09 21:30:05 +0500

    feat: add file for validation

---
+++ src/utils/validation.js
@@ -0,0 +1,45 @@
+import { EMAIL_REGEX } from './constants';
+import { LAT_REGEX, LON_REGEX } from './constants';
+
+const emailRegexp = new RegExp(EMAIL_REGEX, 'i')
+const latRegex = new RegExp(LAT_REGEX);
+const lonRegex = new RegExp(LON_REGEX);
+
+const loginFormValidate = values => {
+  const errors = {};
+
+  if (!values.email) {
+    errors.email = 'Поле должно быть заполнено';
+  } else if (!emailRegexp.test(values.email)) {
+    errors.email = 'Некорректный адрес электронной почты';
+  } 
+  
+  if (!values.password) {
+    errors.password = 'Поле должно быть заполнено';
+  } 
+  
+  return errors;
+};
+
+const cityFormValidate = values => {  
+  const errors = {};
+
+   if (!values.cityName) {
+     errors.cityName = 'Поле должно быть заполнено';
+   }
+
+   if (!latRegex.test(values.latitude)) {
+     errors.latitude = 'Некорректное значение';
+   }
+   
+   if (!lonRegex.test(values.longitude)) {
+     errors.longitude = 'Некорректное значение';
+   } 
+
+   return errors;
+ };
+
+export {
+  loginFormValidate,
+  cityFormValidate,
+}
