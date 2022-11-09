1637ab6cf0ea7b16d016d2cfde66fe18aafec2c5
Ekaterina Epiphanova <epiph@yandex.ru>
2022-11-09 21:29:42 +0500

    feat: add file for constants

---
+++ src/utils/constants.js
@@ -0,0 +1,22 @@
+const BASE_URL = 'http://volker.stairenx.com:81';
+const PROXY = 'http://cors-anywhere.herokuapp.com/';
+
+const credentials = {
+  email: 'stairenx@yandex.ru',
+  password: '89129384461'
+}
+
+const EMAIL_REGEX = '^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}$';
+
+const LAT_REGEX = '^[-+]?([1-8]?\\d(\\.\\d+)?|90(\\.0+)?)$';
+
+const LON_REGEX = '^[-+]?(180(\\.0+)?|((1[0-7]\\d)|([1-9]?\\d))(\\.\\d+)?)$'
+
+
+export {
+  BASE_URL,
+  PROXY,
+  EMAIL_REGEX,
+  LAT_REGEX,
+  LON_REGEX
+}
