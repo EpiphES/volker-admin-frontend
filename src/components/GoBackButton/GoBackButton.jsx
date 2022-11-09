e8b4f28700415f8342a62061eff37b9d6f386acb
Ekaterina Epiphanova <epiph@yandex.ru>
2022-11-09 21:34:25 +0500

    feat: create go back button

---
+++ src/components/GoBackButton/GoBackButton.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from 'react-bootstrap';
+import { BsArrowLeft } from 'react-icons/bs';
+
+function GoBackButton() {
+  const navigate = useNavigate();
+
+  function goBack() {
+    navigate(-1);
+  }
+  
+  return (
+    <Button variant='light' className='d-flex align-items-center gap-2' onClick={goBack}>
+      <BsArrowLeft />
+      Назад     
+    </Button>
+  )
+}
+
+export default GoBackButton
