23e76bac70ca4dd31fab1f471c59a7bb72a478ae
Ekaterina Epiphanova <epiph@yandex.ru>
2022-11-11 11:32:28 +0500

    fix: change check response for delete and update city functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import { BASE_URL, PROXY } from "./constants";
 
 function checkResponse(res) {
-  return res.ok ? res.json() || 'ok' : Promise.reject(`Ошибка: ${res.status}, ${res.statusText}`);
+  return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}, ${res.statusText}`);
 }
 
 function checkSuccess(res) {
@@ -25,7 +25,7 @@ export function login({ email, password }) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ email, password })
-  })  
+  });  
 }
 
 export function getUserInfo(token) {
@@ -34,7 +34,7 @@ export function getUserInfo(token) {
     headers: {
       'Authorization': `Bearer ${token}`,
     }
-  })
+  });
 }
 
 export function getAllCities(isPublished) {
@@ -43,7 +43,7 @@ export function getAllCities(isPublished) {
     headers: {
       'Authorization': `Bearer ${localStorage.getItem('token')}`
     }
-  })
+  });
 }
 
 export function getCityById(id) {
@@ -52,7 +52,7 @@ export function getCityById(id) {
     headers: {
       'Authorization': `Bearer ${localStorage.getItem('token')}`
     }
-  })
+  });
 }
 
 export function createCity({cityName, latitude, longitude, description, modes}) {
@@ -63,27 +63,39 @@ export function createCity({cityName, latitude, longitude, description, modes}) 
       'Authorization': `Bearer ${localStorage.getItem('token')}`,
     },
     body: JSON.stringify({cityName, latitude, longitude, description, modes})
-  })  
+  });  
 }
 
 export function updateCity({id, cityName, latitude, longitude, description, modes}) {
-  return request(`${PROXY}${BASE_URL}/City/Update?id=${id}`, {
+  return fetch(`${PROXY}${BASE_URL}/City/Update?id=${id}`, {
     method: 'POST',
     headers: { 
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${localStorage.getItem('token')}`,
     },
     body: JSON.stringify({cityName, latitude, longitude, description, modes})
-  })  
+  })
+  .then((res) => res.ok ? res : Promise.reject(`Ошибка: ${res.status}, ${res.statusText}`)); 
 }
 
 export function deleteCity(id) {
-  return request(`${PROXY}${BASE_URL}/City/Delete?id=${id}`, {
+  return fetch(`${PROXY}${BASE_URL}/City/Delete?id=${id}`, {
     method: 'DELETE',
     headers: { 
       'Authorization': `Bearer ${localStorage.getItem('token')}`,
     }
-  })  
+  })
+  .then((res) => res.ok ? res : Promise.reject(`Ошибка: ${res.status}, ${res.statusText}`));  
+}
+
+export function removeModeFromCity(cityId, modeId) {
+  return fetch(`${PROXY}${BASE_URL}/City/RemoveModeFromCity?cityId=${cityId}&modeId=${modeId}`, {
+    method: 'DELETE',
+    headers: { 
+      'Authorization': `Bearer ${localStorage.getItem('token')}`,
+    }
+  })
+  .then((res) => res.ok ? res : Promise.reject(`Ошибка: ${res.status}, ${res.statusText}`));  
 }
 
 export function getAllModes() {
