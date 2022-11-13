9742440e268665f3927bfd9cac6152c628d10010
Ekaterina Epiphanova <epiph@yandex.ru>
2022-11-13 12:57:55 +0500

    fix: mode card layout

--- src/components/ModeCard/ModeCard.jsx
+++ src/components/ModeCard/ModeCard.jsx
@@ -1,19 +1,22 @@
 import { Card } from 'react-bootstrap';
 import { MdDelete } from 'react-icons/md';
 
-function ModeCard({mode, onClick, edit}) {
+function ModeCard({item, onClick, edit}) {
   return (
-    <Card 
-      as='button'
-      type='button' 
-      className='shadow-sm w-100 h-100 justify-content-between align-items-center gap-2 p-2 overflow-auto' 
+    <Card      
+      className='shadow-sm w-100 h-100 text-center' 
       border='success'
-      onClick={() => onClick(mode.id)}>
+      onClick={() => onClick(item.id)}
+      style={{cursor: 'pointer'}}>
+
       {!edit && <MdDelete className='position-absolute top-0 end-0 p-1' size={30} />}
-      <Card.Img src={mode.icon} className='w-50'/>          
-      <Card.Text as='h6'>
-        <small>{mode.title}</small>
-      </Card.Text>        
+
+      <Card.Body className=''>
+        <Card.Img src={item.icon} className='w-50' />                
+        <Card.Text as='h6' className='mt-2'>
+          <small>{item.title}</small>
+        </Card.Text>
+      </Card.Body>        
     </Card>
   )
 }
