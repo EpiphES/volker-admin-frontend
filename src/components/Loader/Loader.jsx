231b41f6fdf92c0cf145a5fd591f9d883442ac8b
Ekaterina Epiphanova <epiph@yandex.ru>
2022-11-14 20:33:01 +0500

    fix: change quotes

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,8 +3,8 @@ import { Spinner } from "react-bootstrap";
 function Loader() {
   return (
     <div className='d-flex justify-content-center my-3'>
-      <Spinner animation="border" role="status" className='mx-auto'>
-        <span className="visually-hidden">Loading...</span>
+      <Spinner animation='border' role='status' className='mx-auto'>
+        <span className='visually-hidden'>Loading...</span>
       </Spinner>
     </div>
   )
