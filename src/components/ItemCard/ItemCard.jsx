974756510b346ba4bf46b7bc9ff48a6bada67a03
Ekaterina Epiphanova <epiph@yandex.ru>
2022-11-15 17:39:00 +0500

    refactor: make ItemCard component for modes and types

---
+++ src/components/ItemCard/ItemCard.jsx
@@ -0,0 +1,26 @@
+import { Card } from 'react-bootstrap';
+import { MdDelete } from 'react-icons/md';
+
+function ItemCard({item, onClick, type, deleteOn}) {
+  return (
+    <Card      
+      className='shadow-sm w-100 h-100 ' 
+      border='success'
+      onClick={() => onClick(item.id)}
+      style={{cursor: 'pointer'}}>
+
+      { deleteOn && <MdDelete className='position-absolute top-0 end-0 p-1' size={30} /> }
+
+      { type === 'type' && <Card.Header style={{backgroundColor: item.colorOnMap}} />}
+
+      <Card.Body className='d-flex flex-column align-items-center gap-2 text-center'>
+        <Card.Img src={type === 'type' ? item.iconOnMap : item.icon} className='w-50' />                
+        <Card.Text as='h6' className='mt-auto w-100' style={{textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden'}}>
+          <small >{item.title}</small>
+        </Card.Text>
+      </Card.Body>        
+    </Card>
+  )
+}
+
+export default ItemCard
