e43618f5c7046b6ba5128ea68d2b0e0fcd9af880
Ekaterina Epiphanova <epiph@yandex.ru>
2022-11-15 17:42:24 +0500

    feat: type gallery component

--- src/components/TypesGallery/TypesGallery.jsx
+++ src/components/TypesGallery/TypesGallery.jsx
@@ -1,33 +1,27 @@
-import { Card, Row, Col } from 'react-bootstrap';
-import { MdEdit } from 'react-icons/md';
+import { useSelector } from 'react-redux';
+import { Row, Col } from 'react-bootstrap';
 
+import ItemCard from '../ItemCard/ItemCard';
 import AddCard from '../AddCard/AddCard';
 
-function TypesGallery({types, onCardClick, onAddClick}) {
-  const typeCards = types.map(item => {
+function TypesGallery({onCardClick, onAddClick}) {
+
+  const { currentMode } = useSelector(state => state.mode);
+
+  const typeCards = currentMode.markerTypes.map(item => {
     return (
-      <Col key={item.id}>        
-        <Card 
-          className='shadow-sm w-100 h-100' 
-          border='success'
-          onClick={() => onCardClick(item.id)}
-          style={{cursor: 'pointer'}}>
-          <MdEdit className='position-absolute bottom-0 end-0 p-1' size={30} />
-          <Card.Header style={{backgroundColor: item.colorOnMap}} />
-          <Card.Body>          
-            <Card.Img src={item.iconOnMap} className='w-50'/>        
-            <Card.Text as='h6'>
-              <small>{item.title}</small>
-            </Card.Text> 
-          </Card.Body>       
-        </Card>
+      <Col key={item.id}>
+        <ItemCard 
+          item={item} 
+          onClick={onCardClick}
+          type='type' />      
       </Col>  
     )
-  });
+  });    
 
   return (
-    <section>
-      <h6 className='mb-3'>Типы</h6>
+    <>     
+      <h5 className='mb-3 text-center'>Типы</h5>
       <Row xs={3} sm={4} md={5} lg={6}className='g-2 h-100 mb-3'>
         {typeCards}
         <Col> 
@@ -35,8 +29,8 @@ function TypesGallery({types, onCardClick, onAddClick}) {
             minHeight={'100px'} 
             onClick={onAddClick}/>
         </Col>     
-      </Row>
-    </section>
+      </Row>      
+    </>
   )
 }
 
