520b4a6325ba52f62257d56c932b20590666fa8b
Ekaterina Epiphanova <epiph@yandex.ru>
2022-11-16 21:29:10 +0500

    feat: foll create mode component

--- src/components/CreateMode/CreateMode.jsx
+++ src/components/CreateMode/CreateMode.jsx
@@ -1,9 +1,63 @@
-import GoBackButton from "../GoBackButton/GoBackButton"
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Card } from 'react-bootstrap';
+
+import { createMode } from '../../store/modeSlice';
+
+import * as api from '../../utils/api';
+import { BASE_URL } from '../../utils/constants';
+
+import GoBackButton from '../GoBackButton/GoBackButton';
+import ModeForm from '../ModeForm/ModeForm';
 
 function CreateMode() {
+  const dispatch = useDispatch();
+
+  const [fileLoading, setFileLoading] = useState(false);
+  const [uploadFileError, setUploadFileError] = useState(null);
+  const [showCreateModeMessage, setShowCreateModeMessage] = useState(false);
+  const [showUploadFileError, setShowUploadFileError] = useState(false);
+
+  const {  
+    createModeStatus, 
+    createModeError,  
+  } = useSelector(state => state.mode);
+
+  function handleCreateMode({title, iconFile}) {
+    if(iconFile) {
+      setUploadFileError(null);
+      setFileLoading(true);
+      api.uploadFile(iconFile)
+      .then((res) => {
+        const iconUrl = BASE_URL + res;
+        dispatch(createMode({title, icon: iconUrl}));
+        setShowCreateModeMessage(true);
+      })
+      .catch((err) => {
+        setUploadFileError(err);
+        setShowUploadFileError(true);
+      })
+      .finally(() => setFileLoading(false));
+    } else {
+      dispatch(createMode({title}));
+      setShowCreateModeMessage(true);
+    }       
+  }
+
   return (
     <>
       <GoBackButton />
+      <Card 
+        className='shadow-sm mb-3 mt-2 p-3'
+        border='primary'>
+        <ModeForm 
+          name='create'          
+          buttonText='Создать режим'
+          onSubmit={handleCreateMode}
+          fileLoading={fileLoading}
+        >
+        </ModeForm>
+      </Card>
     </>
   )
 }
