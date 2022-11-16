4d75d653d314062a0912c79666eefe37e2842e95
Ekaterina Epiphanova <epiph@yandex.ru>
2022-11-16 13:29:15 +0500

    fix: change number of columns

--- src/components/TypesGallery/TypesGallery.jsx
+++ src/components/TypesGallery/TypesGallery.jsx
@@ -1,20 +1,21 @@
 import { useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 
-import ItemCard from '../ItemCard/ItemCard';
+import TypeCard from '../TypeCard/TypeCard';
 import AddCard from '../AddCard/AddCard';
 
-function TypesGallery({onCardClick, onAddClick}) {
+function TypesGallery({onUpdate, onDelete, onAddClick}) {
 
   const { currentMode } = useSelector(state => state.mode);
 
   const typeCards = currentMode.markerTypes.map(item => {
     return (
       <Col key={item.id}>
-        <ItemCard 
+        <TypeCard 
           item={item} 
-          onClick={onCardClick}
-          type='type' />      
+          onUpdate={onUpdate}
+          onDelete={onDelete}
+          />      
       </Col>  
     )
   });    
@@ -22,7 +23,7 @@ function TypesGallery({onCardClick, onAddClick}) {
   return (
     <>     
       <h5 className='mb-3 text-center'>Типы</h5>
-      <Row xs={3} sm={4} md={5} lg={6}className='g-2 h-100 mb-3'>
+      <Row xs={2} sm={3} md={4} lg={5}className='g-2 h-100 mb-3'>
         {typeCards}
         <Col> 
           <AddCard 
