38387ef661ab19cc79315960c001ba56c7044e5a
Ekaterina Epiphanova <epiph@yandex.ru>
2022-11-16 21:28:14 +0500

    refactor: transfer functions and messages fron update mode to types section

--- src/components/TypesSection/TypesSection.jsx
+++ src/components/TypesSection/TypesSection.jsx
@@ -1,19 +1,42 @@
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Modal } from 'react-bootstrap';
+
+import { createType, updateType, deleteType } from '../../store/modeSlice';
+
+import * as api from '../../utils/api';
+import { BASE_URL } from '../../utils/constants';
 
 import TypesGallery from '../TypesGallery/TypesGallery';
 import TypeForm from '../TypeForm/TypeForm';
 import ConfirmationPopup from '../ConfirmationPopup/ConfirmationPopup';
+import Message from '../Message/Message';
 
-function TypesSection({onUpdateType, onCreateType, onDeleteType, fileLoading}) {
+function TypesSection({modeId}) {
+  const dispatch = useDispatch();
+  
+  const [selectedType, setSelectedType] = useState(null); 
+  const [deletedType, setDeletedType] = useState('');
   const [showUpdateModal, setShowUpdateModal] = useState(false);
   const [showCreateModal, setShowCreateModal] =
   useState(false);
-  const [selectedType, setSelectedType] = useState(null);
-  const { currentMode } = useSelector(state => state.mode);
   const [showConfirmModal, setShowConfirmModal] = useState(false);
-  const [deletedType, setDeletedType] = useState('');
+  const [showCreateTypeMessage, setShowCreateTypeMessage] = useState(false);
+  const [showUpdateTypeMessage, setShowUpdateTypeMessage] = useState(false);  
+  const [showDeleteTypeMessage, setShowDeleteTypeMessage] = useState(false);
+  const [showUploadFileError, setShowUploadFileError] = useState(false);
+  const [uploadFileError, setUploadFileError] = useState(null);
+  const [fileLoading, setFileLoading] = useState(false);
+
+  const { 
+    currentMode, 
+    createTypeStatus, 
+    createTypeError,
+    updateTypeStatus, 
+    updateTypeError,
+    deleteTypeStatus, 
+    deleteTypeError,  
+  } = useSelector(state => state.mode);
 
   function handleCloseUpdateModal() {
     setShowUpdateModal(false);
@@ -39,10 +62,67 @@ function TypesSection({onUpdateType, onCreateType, onDeleteType, fileLoading}) {
     setDeletedType(currentMode.markerTypes.find((item) => item.id === id));
   };
 
-  function handleDeleteType() {
-    onDeleteType();
-    setShowConfirmModal(false);
+  function handleCreateType({title, colorOnMap, iconFile}) {
+    if(iconFile) {
+      setFileLoading(true);
+      api.uploadFile(iconFile)
+      .then((res) => {
+        const iconUrl = BASE_URL + res;
+        dispatch(createType({
+          markerModeId: modeId,
+          title,
+          colorOnMap,
+          iconOnMap: iconUrl,
+        }));
+        setShowCreateTypeMessage(true)
+      })
+      .catch((err) => setUploadFileError(err))
+      .finally(() => setFileLoading(false));
+    } else {
+      dispatch(createType({
+        markerModeId: modeId,
+        title,
+        colorOnMap,
+      }));
+      setShowCreateTypeMessage(true);
+    }    
   }
+
+  function handleUpdateType({id, title, iconFile, colorOnMap, prevIcon}) {
+    if(iconFile) {
+      setFileLoading(true);
+      api.uploadFile(iconFile)
+      .then((res) => {
+        const iconUrl = BASE_URL + res;
+        dispatch(updateType({
+          id,
+          markerModeId: +modeId,
+          title,
+          colorOnMap,
+          iconOnMap: iconUrl,
+          prevIcon,
+        }));
+        setShowUpdateTypeMessage(true);
+      })
+      .catch((err) => setUploadFileError(err))
+      .finally(() => setFileLoading(false));
+    } else {
+      dispatch(updateType({
+        id,
+        markerModeId: +modeId,
+        title,
+        colorOnMap,
+        iconOnMap: prevIcon,
+      }));
+      setShowUpdateTypeMessage(true);
+    }        
+  }
+  
+  function handleDeleteType() {
+    dispatch(deleteType({id: deletedType.id, prevIcon: deletedType.iconOnMap}));
+    setShowConfirmModal(false);
+    setShowDeleteTypeMessage(true);
+  } 
 
   return (
     <>
@@ -55,7 +135,7 @@ function TypesSection({onUpdateType, onCreateType, onDeleteType, fileLoading}) {
       </section>
 
       <Modal show={showUpdateModal} onHide={handleCloseUpdateModal}>
-        <Modal.Header closeButton>
+        <Modal.Header closeButton className='py-2 '>
           <Modal.Title as='h5'>Тип "{selectedType?.title}"</Modal.Title>
         </Modal.Header>
         <Modal.Body >
@@ -63,32 +143,48 @@ function TypesSection({onUpdateType, onCreateType, onDeleteType, fileLoading}) {
             name='update'
             type={selectedType}
             buttonText='Обновить'
-            onSubmit={onUpdateType}
+            onSubmit={handleUpdateType}
             fileLoading={fileLoading}
-          />          
+          />        
         </Modal.Body>
       </Modal>
 
       <Modal show={showCreateModal} onHide={handleCloseCreateModal}>
-        <Modal.Header closeButton>
+        <Modal.Header closeButton className='py-2 '>
           <Modal.Title as='h5'>Создание типа</Modal.Title>
         </Modal.Header>
         <Modal.Body >
           <TypeForm 
             name='create'
             buttonText='Создать'
-            onSubmit={onCreateType}
-            fileLoading={fileLoading}/>
+            onSubmit={handleCreateType}
+            fileLoading={fileLoading}
+          />
         </Modal.Body>
       </Modal>
 
       <ConfirmationPopup 
-        text={`Удалить режим "${deletedType?.title}"?`}
+        text={`Удалить "${deletedType?.title}"?`}
         show={showConfirmModal}
         onClose={handleCloseConfirmModal}
         onConfirm={handleDeleteType}
         onDecline={handleCloseConfirmModal}
       />
+
+      {createTypeStatus === 'rejected' && <Message type='danger' text={`${createTypeError}`} show={showCreateTypeMessage} setShow={setShowCreateTypeMessage} />}
+
+      {createTypeStatus === 'resolved' && <Message type='success' text='Тип создан!' show={showCreateTypeMessage} setShow={setShowCreateTypeMessage} />}
+
+      {updateTypeStatus === 'rejected' && 
+      <Message type='danger' text={`${updateTypeError}`} show={showUpdateTypeMessage} setShow={setShowUpdateTypeMessage} />}
+
+      {
+      updateTypeStatus === 'resolved' && 
+      <Message type='success' text='Тип обновлен!' show={showUpdateTypeMessage} setShow={setShowUpdateTypeMessage} />}
+
+      {deleteTypeStatus === 'rejected' && <Message type='danger' text={`${deleteTypeError}`} show={showDeleteTypeMessage} setShow={setShowDeleteTypeMessage} />}
+
+      {uploadFileError && <Message type='danger' text={`${uploadFileError}`} show={showUploadFileError} setShow={setShowUploadFileError} />}      
     </>
   )
 }
--- src/components/UpdateMode/UpdateMode.jsx
+++ src/components/UpdateMode/UpdateMode.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { Alert, Card, Button } from 'react-bootstrap';
 
-import { getModeById, updateMode, deleteMode, createType, updateType } from '../../store/modeSlice';
+import { getModeById, updateMode, deleteMode, updateType, deleteType } from '../../store/modeSlice';
 
 import * as api from '../../utils/api';
 import { BASE_URL } from '../../utils/constants';
@@ -16,31 +16,27 @@ import Loader from "../Loader/Loader";
 import ConfirmationPopup from "../ConfirmationPopup/ConfirmationPopup";
 import Message from '../Message/Message';
 
-function UpdateMode({showDeleteMessage}) {
+function UpdateMode({showDeleteModeMessage}) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const {modeId} = useParams();
   const [showConfirmModal, setShowConfirmModal] = useState(false);  
   const [showUpdateModeMessage, setShowUpdateModeMessage] = useState(false);
-  const [showUpdateTypeMessage, setShowUpdateTypeMessage] = useState(false);
-  const [showCreateTypeMessage, setShowCreateTypeMessage] = useState(false);
+  const [showUploadFileError, setShowUploadFileError] = useState(false);
   const [uploadFileError, setUploadFileError] = useState(null);
-  const [fileLoading, setFileLoading] = useState(true);
+  const [fileLoading, setFileLoading] = useState(false);
 
   const { 
     currentMode, 
     currentModeStatus, 
     currentModeError, 
     updateModeStatus, 
-    updateModeError,
-    createTypeStatus, 
-    createTypeError,
-    updateTypeStatus, 
-    updateTypeError,  
+    updateModeError, 
   } = useSelector(state => state.mode); 
 
   function handleUpdateMode({title, iconFile }) {
     if(iconFile) {
+      setUploadFileError(null);
       setFileLoading(true);
       api.uploadFile(iconFile)
       .then((res) => {
@@ -48,8 +44,11 @@ function UpdateMode({showDeleteMessage}) {
         dispatch(updateMode({id: modeId, title, icon: iconUrl, prevIcon: currentMode.icon}));
         setShowUpdateModeMessage(true);
       })
-      .catch((err) => console.log(err))
-      .finally(setFileLoading(false));
+      .catch((err) => {
+        setUploadFileError(err);
+        setShowUploadFileError(true);
+      })
+      .finally(() => setFileLoading(false));
     } else {
       dispatch(updateMode({id: modeId, title, icon: currentMode.icon}));
       setShowUpdateModeMessage(true);
@@ -59,77 +58,21 @@ function UpdateMode({showDeleteMessage}) {
   function handleCloseConfirmModal() {
     setShowConfirmModal(false);
   }
+
   function handleShowConfirmModal() {
     setShowConfirmModal(true);
-  };
+  }
 
   function handleDeleteMode() {
-    dispatch(deleteMode({id: modeId, prevIcon: currentMode.icon}));
-    handleCloseConfirmModal();
-    navigate('/modes');
-    showDeleteMessage();
+    if(currentMode.markerTypes.length === 0) { 
+      dispatch(deleteMode({id: modeId, prevIcon: currentMode.icon}));
+      handleCloseConfirmModal();
+      navigate('/modes');
+      showDeleteModeMessage(true);
+    }
   }
   
-  function handleCreateType({title, colorOnMap, iconFile}) {
-    if(iconFile) {
-      setFileLoading(true);
-      api.uploadFile(iconFile)
-      .then((res) => {
-        const iconUrl = BASE_URL + res;
-        dispatch(createType({
-          markerModeId: +modeId,
-          title,
-          colorOnMap,
-          iconOnMap: iconUrl,
-        }));
-        setShowCreateTypeMessage(true)
-      })
-      .catch((err) => setUploadFileError(err))
-      .finally(setFileLoading(false));
-    } else {
-      dispatch(createType({
-        markerModeId: +modeId,
-        title,
-        colorOnMap,
-      }));
-      setShowCreateTypeMessage(true);
-    }    
-    
-  }
   
-  function handleUpdateType({id, title, iconFile, colorOnMap, prevIcon}) {
-    if(iconFile) {
-      setFileLoading(true);
-      api.uploadFile(iconFile)
-      .then((res) => {
-        const iconUrl = BASE_URL + res;
-        dispatch(updateType({
-          id,
-          markerModeId: +modeId,
-          title,
-          colorOnMap,
-          iconOnMap: iconUrl,
-          prevIcon,
-        }));
-        setShowUpdateTypeMessage(true);
-      })
-      .catch((err) => setUploadFileError(err))
-      .finally(setFileLoading(false));
-    } else {
-      dispatch(updateType({
-        id,
-        markerModeId: +modeId,
-        title,
-        colorOnMap,
-        iconOnMap: prevIcon,
-      }));
-      setShowUpdateTypeMessage(true);
-    }        
-  }
-
-  function handleDeleteType() {
-
-  } 
 
   useEffect(() => {
     dispatch(getModeById(modeId));    
@@ -156,12 +99,7 @@ function UpdateMode({showDeleteMessage}) {
         <Card 
           className='shadow-sm mb-4 mt-2 p-3'
           border='primary'> 
-          <TypesSection 
-            onUpdateType={handleUpdateType}
-            onCreateType={handleCreateType}
-            onDeleteType={handleDeleteType}
-            fileLoading={fileLoading}
-          />
+          <TypesSection modeId={+modeId}/>
         </Card>
 
         <Button
@@ -169,12 +107,12 @@ function UpdateMode({showDeleteMessage}) {
           type='button'
           size='lg'
           aria-label='удалить режим'
-          disabled={currentMode.markerTypes.length > 0}
+          disabled={currentMode?.markerTypes.length > 0}
           onClick={handleShowConfirmModal}
           className='d-block m-auto'>
           Удалить режим
         </Button>
-        { currentMode.markerTypes.length > 0 && <p className='text-danger text-center'>Чтобы удалить режим, сначала удалите все типы</p> }
+        { currentMode?.markerTypes.length > 0 && <p className='text-danger text-center'>Чтобы удалить режим, сначала удалите все типы</p> }
           
 
         <GoBackButton />                
@@ -193,17 +131,11 @@ function UpdateMode({showDeleteMessage}) {
         onDecline={handleCloseConfirmModal}
       />
 
-      {updateModeStatus === 'rejected' && <Message type='error' text={`${updateModeError}`} show={showUpdateModeMessage} setShow={setShowUpdateModeMessage} />}
+      {updateModeStatus === 'rejected' && <Message type='danger' text={`${updateModeError}`} show={showUpdateModeMessage} setShow={setShowUpdateModeMessage} />}
 
       {updateModeStatus === 'resolved' && <Message type='success' text='Режим обновлен!' show={showUpdateModeMessage} setShow={setShowUpdateModeMessage} />}
 
-      {updateTypeStatus === 'rejected' && <Message type='error' text={`${updateTypeError}`} show={showUpdateTypeMessage} setShow={setShowUpdateTypeMessage} />}
-
-      {updateTypeStatus === 'resolved' && <Message type='success' text='Тип обновлен!' show={showUpdateTypeMessage} setShow={setShowUpdateTypeMessage} />}
-
-      {createTypeStatus === 'rejected' && <Message type='error' text={`${createTypeError}`} show={showCreateTypeMessage} setShow={setShowCreateTypeMessage} />}
-
-      {createTypeStatus === 'resolved' && <Message type='success' text='Тип создан!' show={showCreateTypeMessage} setShow={setShowCreateTypeMessage} />}                  
+      {uploadFileError && <Message type='danger' text={`${uploadFileError}`} show={showUploadFileError} setShow={setShowUploadFileError} />}                
     </>
   )
 }
