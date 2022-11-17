fc089ceded75091ea334c8bb4431ec9033df6296
Ekaterina Epiphanova <epiph@yandex.ru>
2022-11-17 20:39:14 +0500

    feat: create marker card

---
+++ src/components/MarkerCard/MarkerCard.jsx
@@ -0,0 +1,31 @@
+import { Card } from 'react-bootstrap';
+
+function MarkerCard({item, onClick}) {
+  const markerTypesString = item.markerTypes.map(i => i.title).join(', ');
+  return (
+    <Card      
+      className='shadow-sm w-100 h-100 ' 
+      border='success'
+      onClick={() => onClick(item.id)}
+      style={{cursor: 'pointer'}}>
+      <Card.Img 
+        src={item.images[0]} 
+        variant='top'
+        style={{height: '150px', objectFit: 'cover'}} 
+      />
+      <Card.Body className='p-2'>                     
+        <Card.Text style={{fontSize: '12px', lineHeight: 1.2}} className='mb-1' >
+          <strong>{item.title}</strong>
+        </Card.Text>
+        <Card.Text style={{fontSize: '12px', lineHeight: 1.2}} className='mb-1 text-secondary' >
+          {markerTypesString}
+        </Card.Text>
+        <Card.Text style={{fontSize: '12px', lineHeight: 1.2, textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden'}}>
+          {item.address}
+        </Card.Text>
+      </Card.Body>         
+    </Card>    
+  )
+}
+
+export default MarkerCard;
