2de55d8c5ec6db8ce779e8119b253f5d79b6bac4
Ekaterina Epiphanova <epiph@yandex.ru>
2022-11-17 20:38:15 +0500

    feat: create markers gallery

---
+++ src/components/MarkersGallery/MarkersGallery.jsx
@@ -0,0 +1,65 @@
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Row, Col, Alert } from 'react-bootstrap';
+
+import MarkerCard from '../MarkerCard/MarkerCard';
+import Loader from '../Loader/Loader';
+import AddCard from '../AddCard/AddCard';
+import BtnScrollUp from '../BtnScrollUp/BtnScrollUp';
+
+function MarkersGallery() {
+  const navigate = useNavigate();
+  const { 
+    markers, 
+    getMarkersStatus,
+    getMarkersError, 
+  } = useSelector(state => state.marker);
+  const {currentCity} = useSelector(state => state.city);
+  
+
+  const markerCards = markers.map(item => {
+    return (
+      <Col key={item.id}>        
+        <MarkerCard item={item} onClick={handleCardClick} />
+      </Col>  
+    )
+  });
+
+  function handleCardClick(markerId) {
+    navigate(`${markerId}`);
+  }
+
+  function handleAddClick() {
+    navigate('create');
+  }
+
+  return (
+    <>
+      <section className='mt-3'>
+        { !currentCity && 
+          <Alert variant='primary'>
+            Выберите город в разделе "Профиль"
+          </Alert> }
+        { getMarkersStatus ==='loading' && <Loader /> }
+        { getMarkersStatus ==='resolved' && markers.length === 0 && 
+        <Alert variant='primary'>
+          В текущем городе еще нет ни одного маркера.
+        </Alert> }
+        { getMarkersStatus === 'rejected' && 
+          <Alert variant='danger'>
+            {getMarkersError}
+          </Alert> }
+        { getMarkersStatus ==='resolved' && currentCity &&
+        <Row xs={2} sm={3} md={4} lg={5} className='g-2 h-100 mb-3'>
+          <Col>
+            <AddCard minHeight='150px' onClick={handleAddClick} type='mode'/>
+          </Col>
+          {markerCards}        
+        </Row> }            
+      </section>
+      <BtnScrollUp />    
+    </>
+  )
+}
+
+export default MarkersGallery;
