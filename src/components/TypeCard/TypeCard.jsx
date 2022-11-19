e9b969cd05c538f5722432b5ffa8593038259cab
Ekaterina Epiphanova <epiph@yandex.ru>
2022-11-19 14:41:02 +0500

    fix: add place prop and different layout for different place

--- src/components/TypeCard/TypeCard.jsx
+++ src/components/TypeCard/TypeCard.jsx
@@ -1,6 +1,7 @@
 import { Card, Dropdown } from 'react-bootstrap';
+import { MdDelete } from 'react-icons/md';
 
-function TypeCard({item, onUpdate, onDelete}) {
+function TypeCard({item, onUpdate, onDelete, place}) {
   return (
     <Card      
       className='shadow-sm w-100 h-100 ' 
@@ -12,17 +13,20 @@ function TypeCard({item, onUpdate, onDelete}) {
         <Card.Img src={item.iconOnMap} className='w-50' />                
         <Card.Text as='h6' className='mt-auto w-100' style={{textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden'}}>
           <small >{item.title}</small>
-        </Card.Text>        
-        <Dropdown>
-          <Dropdown.Toggle variant="success">
-            Изменить
-          </Dropdown.Toggle>
+        </Card.Text>
+        { place === 'marker' && <MdDelete className='position-absolute top-0 end-0 p-1' size={30} />}
+        { place === 'mode' &&
+          <Dropdown>
+            <Dropdown.Toggle variant="success">
+              Изменить
+            </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            <Dropdown.Item onClick={() => onUpdate(item.id)}>Редактировать</Dropdown.Item>
-            <Dropdown.Item onClick={() => onDelete(item.id)}>Удалить</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown> 
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => onUpdate(item.id)}>Редактировать</Dropdown.Item>
+              <Dropdown.Item onClick={() => onDelete(item.id)}>Удалить</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        } 
       </Card.Body>        
     </Card>
   )
