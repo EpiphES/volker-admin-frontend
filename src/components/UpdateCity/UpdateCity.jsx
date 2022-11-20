506aa4f15b81c6d69af062cfbec48079d92abae6
Ekaterina Epiphanova <epiph@yandex.ru>
2022-11-20 12:57:41 +0500

    feat: add loader and error alert

--- src/components/UpdateCity/UpdateCity.jsx
+++ src/components/UpdateCity/UpdateCity.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Alert } from 'react-bootstrap';
 
 import { updateCity } from '../../store/citySlice';
 
 import CityForm from '../CityForm/CityForm';
 import GoBackButton from '../GoBackButton/GoBackButton';
 import Message from '../Message/Message';
+import Loader from '../Loader/Loader';
 
 function UpdateCity() {
   const {currentCity} = useSelector(state => state.city);
@@ -15,6 +17,8 @@ function UpdateCity() {
     updateCityError, 
     removeModeFromCityStatus,
     removeModeFromCityError,
+    currentCityStatus,
+    currentCityError
   } = useSelector(state => state.city);
   
   const [showMessage, setShowMessage] = useState(false);  
@@ -26,16 +30,24 @@ function UpdateCity() {
 
   return (
     <>
-      <GoBackButton />      
-      
-      <CityForm 
-        name='update' 
-        city={currentCity} 
-        buttonText='Сохранить'
-        onSubmit={handleUpdateCity}
-      />
+      <GoBackButton /> 
+      { currentCityStatus === 'loading' && <Loader />}
+      { currentCityStatus === 'resolved' &&     
+        <>
+          <CityForm 
+            name='update' 
+            city={currentCity} 
+            buttonText='Сохранить'
+            onSubmit={handleUpdateCity}
+          />
 
-      <GoBackButton />
+          <GoBackButton />
+        </>}
+
+      { currentCityStatus === 'rejected' && 
+      <Alert variant='danger'>
+        {currentCityError}
+      </Alert> }
       
       {(updateCityStatus === 'rejected' || removeModeFromCityStatus === 'rejected') && <Message type='danger' text={`${updateCityError || ''} ${removeModeFromCityError || ''}`} show={showMessage} setShow={setShowMessage}/>}
 
