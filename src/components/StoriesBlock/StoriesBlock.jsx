ac9b2baca197d665bbbdf0cc4b811f62846507df
Ekaterina Epiphanova <epiph@yandex.ru>
2022-12-14 12:57:11 +0500

    feat: add buttons

--- src/components/StoriesBlock/StoriesBlock.jsx
+++ src/components/StoriesBlock/StoriesBlock.jsx
@@ -1,12 +1,12 @@
 import { useNavigate } from 'react-router-dom';
 import { Card, Button, Row, Col } from 'react-bootstrap';
 
-import { MdEdit } from 'react-icons/md';
+import { MdEdit, MdDelete } from 'react-icons/md';
 
 import AddCard from '../AddCard/AddCard';
 import StoriesGroup from '../StoriesGroup/StoriesGroup';
 
-function StoriesBlock({item}) {
+function StoriesBlock({item, onUpdate, onDelete}) {
   const navigate = useNavigate();
 
   const storiesGroupCards = item.storiesGroups.map(group => {
@@ -31,16 +31,27 @@ function StoriesBlock({item}) {
       border='primary'
     >
       <Card.Header
-        className='d-flex justify-content-between align-items-center'>
+        className='d-flex align-items-center'>
         <Card.Title>{item.title}</Card.Title>
         <Button
           variant='outline-dark'
-          className='lh-1'
+          className='lh-1 ms-auto'
           type='button'
-          aria-label='добавить блок историй'
+          aria-label='редактировать блок историй'
+          onClick={() => onUpdate(item.id)}
         >
           <MdEdit size={20} />
         </Button>
+        <Button
+          variant='outline-dark'
+          className='lh-1 ms-2'
+          type='button'
+          aria-label='удалить блок историй'
+          onClick={() => onDelete(item.id)}
+        >
+          <MdDelete size={20} />
+        </Button>
+
       </Card.Header>
       <Card.Body>
         <Row xs={2} sm={3} md={4} lg={5} className='g-2 h-100 mb-3'>
