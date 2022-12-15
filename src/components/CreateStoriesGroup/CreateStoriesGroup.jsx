855f3034a858f04974821ff6cb8042b25f4a904d
Ekaterina Epiphanova <epiph@yandex.ru>
2022-12-15 19:53:54 +0500

    feat: fill CreateStoriesGroup component

--- src/components/CreateStoriesGroup/CreateStoriesGroup.jsx
+++ src/components/CreateStoriesGroup/CreateStoriesGroup.jsx
@@ -1,7 +1,59 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Card } from 'react-bootstrap';
+
+import { createStoriesGroup } from '../../store/storySlice';
+
+import GoBackButton from '../GoBackButton/GoBackButton';
+import StoriesGroupForm from '../StoriesGroupForm/StoriesGroupForm';
+import Message from '../Message/Message';
 
 function CreateStoriesGroup() {
+  const dispatch = useDispatch();
+
+  const [showCreateGroupMessage, setShowCreateGroupMessage] = useState(false);
+  
+  const {  
+    createStoriesGroupStatus, 
+    createStoriesGroupError,  
+  } = useSelector(state => state.story);
+
+  function handleCreateGroup({imageUrl, ...values}) {
+    if(imageUrl) {
+      dispatch(createStoriesGroup({
+        image: imageUrl,
+        ...values
+      }));
+      setShowCreateGroupMessage(true);
+    } else {
+      dispatch(createStoriesGroup({
+        values
+      }));
+      setShowCreateGroupMessage(true);
+    }       
+  }
+
   return (
-    <div>CreateStoriesGroup</div>
+    <>
+      <GoBackButton />
+      <Card
+        body 
+        className='shadow-sm mb-3 mt-2 mx-auto'
+        style={{maxWidth: '800px'}}
+        border='primary'>
+        <StoriesGroupForm 
+          name='create'          
+          buttonText='Создать группу'
+          submitHandler={handleCreateGroup}
+        >
+        </StoriesGroupForm>
+      </Card>
+
+      {createStoriesGroupStatus === 'rejected' && <Message type='danger' text={`${createStoriesGroupError}`} show={showCreateGroupMessage} setShow={setShowCreateGroupMessage} />}
+
+      {createStoriesGroupStatus === 'resolved' && <Message type='success' text='Группа создана!' show={showCreateGroupMessage} setShow={setShowCreateGroupMessage} />}
+ 
+    </>
   )
 }
 
