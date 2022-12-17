65033ec0e58d4cd574a54461ecaa4f1d25e44230
Ekaterina Epiphanova <epiph@yandex.ru>
2022-12-17 12:15:59 +0500

    feat: add button to change slide

--- src/components/StorySlider/StorySlider.jsx
+++ src/components/StorySlider/StorySlider.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
-import { Button, Carousel } from 'react-bootstrap';
+import { Button, Carousel, Dropdown } from 'react-bootstrap';
 
-function StoriesSlider() {
+function StoriesSlider({onDelete, onEdit}) {
   const { 
     currentStoriesGroup,  
   } = useSelector(state => state.story);
@@ -13,26 +13,43 @@ function StoriesSlider() {
           className='d-block m-auto'
           src={item.image}
           alt='слайд истории'
-          style={{width: '300px', borderRadius: '0.375rem', overflow: 'hidden'}}
+          style={{width: '280px', borderRadius: '0.375rem', overflow: 'hidden'}}
         />
+        <div className='position-absolute top-0 mt-3 start-0 end-0 d-flex justify-content-center'>
+        <Dropdown >
+          <Dropdown.Toggle variant='light' >
+            Изменить
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={() => onEdit(item)}>Редактировать</Dropdown.Item>
+            <Dropdown.Item onClick={() => onDelete(item)}>Удалить</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        </div>
+        
         <Carousel.Caption>
           <h3>{item.title}</h3>
           <p>{item.message}</p>
           {item.buttonName && 
             <Button
             variant='light'
-            className='opacity-50'
+            className='opacity-50 mb-3'
             >
               {item.buttonName}
             </Button>
-          }          
-        </Carousel.Caption>
+          }                   
+        </Carousel.Caption>        
       </Carousel.Item>
     )
   })
 
-  return (
-    <Carousel interval={null} variant='dark'>
+  return (    
+    <Carousel 
+      interval={null} 
+      variant='dark'
+      className='mb-3'
+      style={{minHeight: '50px'}}>
       {storiesItemCards}
     </Carousel>
   )
