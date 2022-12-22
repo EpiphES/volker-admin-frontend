ffdbbf82ef41d61a46c6fbdf22caf3ee88090c49
Ekaterina Epiphanova <epiph@yandex.ru>
2022-12-22 10:42:08 +0500

    feat: finish markers filter

--- src/components/MarkersFilter/MarkersFilter.jsx
+++ src/components/MarkersFilter/MarkersFilter.jsx
@@ -1,14 +1,11 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useFormik } from 'formik';
-import { Accordion, InputGroup, Form } from 'react-bootstrap';
+import { Accordion, Button, Form, InputGroup } from 'react-bootstrap';
 import { BsSearch } from 'react-icons/bs';
 
-function MarkersFilter() {
+function MarkersFilter({onSubmit, onReset}) {
   const {
-    searchQuery,
-    isPublished,
-    mode,
-    type, 
     fetchMarkersStatus, 
   } = useSelector(state => state.marker);
 
@@ -16,21 +13,32 @@ function MarkersFilter() {
 
   const modeSelect = currentCity.modes.map((mode) => {
     return (<option key={mode.id} value={mode.id}>{mode.title}</option>)
-  })
+  });
 
   const formik = useFormik({
     initialValues: {
-      searchQuery: searchQuery,
+      searchQuery: '',
       mode: '',
       type: '',
-      isPublished: 'all'
+      isPublished: 'all',
     },
     onSubmit: values => {
-      console.log(values);
+      onSubmit(values);
     },
   });
+
+  const typeSelect = currentCity.modes.find((item) => item.id === parseInt(formik.values.mode))?.markerTypes.map((type) => {
+    return (<option key={type.id} value={type.id}>{type.title}</option>)
+  });
+
+  useEffect(() => {
+    if(currentCity) {
+      formik.handleReset();
+    }
+  }, [currentCity]) 
+
   return (
-    <Accordion defaultActiveKey="0" className='mb-3'>
+    <Accordion className='mb-3'>
         <Accordion.Item eventKey="0">
           <Accordion.Header>Фильтры</Accordion.Header>
           <Accordion.Body>
@@ -50,25 +58,28 @@ function MarkersFilter() {
                     aria-label='поиск маркеров'
                     type='text'
                     name='searchQuery'
-                    value={formik.searchQuery}
+                    value={formik.values.searchQuery}
                     onChange={formik.handleChange}
                   />
                 </InputGroup>
-                <div className='mb-3'>
+
+                <Form.Group className='mb-3'>
                   <Form.Check
                     inline
                     label="Опубликованные"
                     name='isPublished'
                     type='radio'
                     value='true'
+                    checked={formik.values.isPublished === 'true'}
                     onChange={formik.handleChange}
                   />
                   <Form.Check
                     inline
-                    label="НеОпубликованные"
+                    label="Неопубликованные"
                     name='isPublished'
                     type='radio'
                     value='false'
+                    checked={formik.values.isPublished === 'false'}
                     onChange={formik.handleChange}
                   />
                   <Form.Check
@@ -77,21 +88,63 @@ function MarkersFilter() {
                     name='isPublished'
                     type='radio'
                     value='all'
+                    checked={formik.values.isPublished === 'all'}
                     onChange={formik.handleChange}
                   />
-                </div>
-                <Form.Select 
-                  aria-label='выберите режим' 
-                  onChange={(e) => {
-                    formik.handleChange(e);
-                    // handleChangeMode(e);
+                </Form.Group>
+
+                <Form.Group className='mb-3'>
+                  <Form.Label className='h6 mb-3' htmlFor={`mode-filter`}>
+                    Выберите режим
+                  </Form.Label>
+                  <Form.Select 
+                    aria-label='выберите режим' 
+                    onChange={(e) => {
+                      formik.handleChange(e);
+                      formik.values.type = '';
+                    }}
+                    name='mode'
+                    id={`mode-filter`}
+                    value={formik.values.mode}>
+                    <option value=''>Вcе</option>
+                    {modeSelect}
+                  </Form.Select>
+                </Form.Group>
+
+                <Form.Group>
+                  <Form.Label className='h6 mb-3' htmlFor={`type-filter`}>
+                    Выберите тип
+                  </Form.Label>
+                  <Form.Select 
+                    aria-label='выберите тип' 
+                    onChange={formik.handleChange}
+                    name='type'
+                    id={`type-filter`}
+                    value={formik.values.type}>
+                    <option value=''>Вcе</option>
+                    {typeSelect}
+                  </Form.Select>
+                </Form.Group>
+                <Button
+                  variant='primary'
+                  type='submit'
+                  aria-label='Применить'
+                  className='mt-4'>
+                  {fetchMarkersStatus === 'loading' ? 'Загрузка...' : 'Применить'}
+                </Button>
+
+                <Button
+                  variant='primary'
+                  type='reset'
+                  aria-label='очистить изменения'
+                  onClick={(e) => {
+                    formik.handleReset(e);
+                    onReset();
                   }}
-                  name='mode'
-                  id={`mode-filter`}
-                  value={formik.values.mode}>
-                  <option value=''>Вcе</option>
-                  {modeSelect}
-                </Form.Select>
+                  className='ms-2 mt-4'>
+                  Сбросить
+                </Button>
+
               </fieldset>
             </Form>            
           </Accordion.Body>
