fb8fa6b29c2da3be2c58fdacbecfa011e3e85bfc
Ekaterina Epiphanova <epiph@yandex.ru>
2022-12-29 18:54:37 +0500

    feat: add card

--- src/components/pages/MainPage/MainPage.jsx
+++ src/components/pages/MainPage/MainPage.jsx
@@ -1,8 +1,16 @@
+import { Card } from 'react-bootstrap';
+
 import PushSection from '../../PushSection/PushSection.jsx';
 
 function Main() {
   return (
-    <PushSection />
+    <Card
+        body
+        className='shadow-sm mb-3 mt-2 mx-auto'
+        border='primary'
+        style={{ maxWidth: '800px' }}>
+      <PushSection />
+    </Card>
   );
 }
 
